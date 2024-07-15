6ef4b8000ef7ed26e0d6bf21ee881ed0ffc4cdbd
Keinazj <jafari.keinaz@gmail.com>
2024-07-15 19:38:22 +0330

    fixed the bugs, add some method

--- src/Treap.java
+++ src/Treap.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Treap<Key extends Comparable<Key>>{
     private Node<Key> root;
     private Node<Key> min;
@@ -52,6 +54,7 @@ public class Treap<Key extends Comparable<Key>>{
      * @param suc The successor of the node.
      * @return A node containing the added key.
      */
+
     private Node<Key> add_helper(Key key, Node<Key> root, Node<Key> pre, Node<Key> suc) {
         if (root == null) {
             Node<Key> newNode = new Node<Key>(key);
@@ -95,8 +98,6 @@ public class Treap<Key extends Comparable<Key>>{
         return root;
     }
 
-
-
     /**
      * returns the k-th smallest element in the Treap.
      @param  k   index + 1 of the desired element.
@@ -117,94 +118,155 @@ public class Treap<Key extends Comparable<Key>>{
         return tmp.getKey();
     }
 
+    /**
+     * this method deletes a key from the treap
+     * @param key the key that should be deleted
+     */
+    public void delete(Key key){
+        this.root = delete_helper(key, this.root);
+        updateMinMax(root); // Update min and max pointers after deletion
+    }
+
+    private Node<Key> delete_helper(Key key, Node<Key> root) {
+        if (root == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(root.getKey());
+        if (cmp < 0) {
+            root.setLeftChild(delete_helper(key, root.getLeftChild()));
+            if (root.getLeftChild() != null) {
+                root.getLeftChild().setParent(root);
+            }
+        } else if (cmp > 0) {
+            root.setRightChild(delete_helper(key, root.getRightChild()));
+            if (root.getRightChild() != null) {
+                root.getRightChild().setParent(root);
+            }
+        } else {
+            // Node to delete found
+            if (root.getLeftChild() == null) {
+                Node<Key> tmp = root.getRightChild();
+                updateSucAndPre(root, null, tmp);
+                root = tmp;
+            } else if (root.getRightChild() == null) {
+                Node<Key> tmp = root.getLeftChild();
+                updateSucAndPre(root, tmp, null);
+                root = tmp;
+            } else {
+                // Node has two children
+                if (root.getLeftChild().getPriority() < root.getRightChild().getPriority()) {
+                    root = root.leftRotate();
+                    root.setLeftChild(delete_helper(key, root.getLeftChild()));
+                    if (root.getLeftChild() != null) {
+                        root.getLeftChild().setParent(root);
+                    }
+                } else {
+                    root = root.rightRotate();
+                    root.setRightChild(delete_helper(key, root.getRightChild()));
+                    if (root.getRightChild() != null) {
+                        root.getRightChild().setParent(root);
+                    }
+                }
+            }
+        }
+
+        // Update subtree size and height
+        if (root != null) {
+            int leftSubtreeSize = (root.getLeftChild() != null) ? root.getLeftChild().getSubtreeSize() : 0;
+            int rightSubtreeSize = (root.getRightChild() != null) ? root.getRightChild().getSubtreeSize() : 0;
+            root.setSubtreeSize(leftSubtreeSize + rightSubtreeSize + 1);
+
+            int leftHeight = (root.getLeftChild() != null) ? root.getLeftChild().getHeightFromBottom() : -1;
+            int rightHeight = (root.getRightChild() != null) ? root.getRightChild().getHeightFromBottom() : -1;
+            root.setHeightFromBottom(Math.max(leftHeight, rightHeight) + 1);
+        }
+
+        return root;
+    }
+
+    /**
+     * this method updates the min and max of the treap after changes
+     * @param root the root
+     */
+
+    // Update min and max pointers after deletion
+    private void updateMinMax(Node<Key> root) {
+        if (root == null) {
+            min = null;
+            max = null;
+            return;
+        }
+
+        min = findMin(root);
+        max = findMax(root);
+    }
+
+    /**
+     * this method finds the minimum key
+     * @param node
+     * @return minimum key
+     */
+
+    private Node<Key> findMin(Node<Key> node) {
+        if (node == null) {
+            return null;
+        }
+        while (node.getLeftChild() != null) {
+            node = node.getLeftChild();
+        }
+        return node;
+    }
+
+    /**
+     * this method finds the maximum key
+     * @param node
+     * @return maximum key
+     */
+    private Node<Key> findMax(Node<Key> node) {
+        if (node == null) {
+            return null;
+        }
+        while (node.getRightChild() != null) {
+            node = node.getRightChild();
+        }
+        return node;
+    }
 
 
-    public void updateSucAndPre(Node<Key> node , Node<Key> pre, Node<Key> suc){
-        if (pre != null){
+    /**
+     *this method updates the successors and predecessors after the changes
+     * @param node
+     * @param pre
+     * @param suc
+     */
+
+    public void updateSucAndPre(Node<Key> node, Node<Key> pre, Node<Key> suc) {
+        if (node == null) {
+            return;
+        }
+        if (pre != null) {
             pre.setSuccessor(node.getSuccessor());
         }
-        if (suc !=null){
+        if (suc != null) {
             suc.setPredecessor(node.getPredecessor());
         }
     }
 
     /**
-     * Delete a key from the given Treap.
-     * @param key key to be removed from the Treap.
-
+     * this method gives the keys of the treap in an ascending order
+     * @return an arraylist of the keys in ascending order
      */
-    public void delete(Key key){
-        this.root = delete_helper(key,this.root);
-        if (root==null)
-            return;
-        if (min==null){
-            while (root.getLeftChild()!=null){
-                min = root.getLeftChild();
-            }
+    public ArrayList<Key> orderedKeys(){
+        ArrayList keys = new ArrayList();
+        Node<Key> tmp = min;
+        while(tmp!=null){
+            keys.add(tmp.getKey());
+            tmp = tmp.getSuccessor();
         }
-        if (max == null){
-            while (root.getRightChild()!=null)
-                max = root.getRightChild();
-        }
-
+        return keys;
     }
-    /**
-     * Helper function to delete a key from a Treap.
-     * @param key Key to be removed from the Treap.
-     * @param root The root of the Treap.
-     * @return returns the root node of the Treap. Null if Treap is empty.
-     */
-    private Node<Key> delete_helper(Key key, Node<Key> root) {
-        if (root == null){
-            return root;
-        }
-        int cmp = key.compareTo(root.getKey());
-        if (cmp < 0){
-            root.setLeftChild(delete_helper(key, root.getLeftChild()));
-            root.getLeftChild().setParent(root);
-            root.setSubtreeSize(root.getSubtreeSize() -1);
-        } else if (cmp > 0) {
-            root.setRightChild(delete_helper(key,root.getRightChild()));
-            root.getRightChild().setLeftChild(root);
-            root.setSubtreeSize(root.getSubtreeSize() -1);
-        }
-        else if (root.getLeftChild()==null){
-            Node<Key> tmp = root.getRightChild();
-            updateSucAndPre(root,null, tmp);
-            root = tmp;
-        }
-        else if (root.getRightChild()==null){
-            Node<Key> tmp = root.getLeftChild();
-            updateSucAndPre(root,tmp,null);
-            root = tmp;
-        }
-        else if (root.getLeftChild().getPriority() < root.getRightChild().getPriority()){
-            root = root.leftRotate();
-            root.setSubtreeSize(root.getSubtreeSize()-1);
-            root.setLeftChild(delete_helper(key,root.getLeftChild()));
-            root.getLeftChild().setParent(root);
 
-        }
-        else{
-            root = root.rightRotate();
-            root.setSubtreeSize(root.getSubtreeSize()-1);
-
-            root.setRightChild(delete_helper(key,root.getRightChild()));
-            root.getRightChild().setParent(root);
-        }
-        if (root !=null) {
-            int height = -1;
-            if (root.getLeftChild() != null) {
-                height = root.getLeftChild().getHeightFromBottom();
-            }
-            if (root.getRightChild() != null && root.getRightChild().getHeightFromBottom() > height) {
-                height = root.getRightChild().getHeightFromBottom();
-            }
-            root.setHeightFromBottom(height + 1);
-        }
-
-        return root;
-    }
 
     /**
      * get the height of the Treap. 0 can either mean that the Treap is empty or has only a single Node in it.
--- src/main.java
+++ src/main.java
@@ -1,16 +1,17 @@
 public class main {
     public static void main(String[] args) {
 
-//        int sum = 0;
-//        for (int j = 0; j< 1000; j++) {
-//            Treap<Integer> treap = new Treap<>();
-//            for (int i = 0; i < 100000; i++) {
-//                treap.add(i);
-//            }
-//            System.out.println(treap.getTreapHeight());
-//            sum += treap.getTreapHeight();
-//        }
-//        System.out.println(sum/1000);
+        int sum = 0;
+        for (int j = 0; j< 1000; j++) {
+            Treap<Integer> treap = new Treap<>();
+            for (int i = 0; i < 100000; i++) {
+                treap.add(i);
+            }
+            System.out.println(treap.getTreapHeight());
+            sum += treap.getTreapHeight();
+        }
+        System.out.println(sum/1000);
+
         Treap<Integer> treap2 = new Treap<>();
         treap2.add(1);
         treap2.add(2);
@@ -19,12 +20,12 @@ public class main {
         treap2.add(35);
         treap2.add(7);
         treap2.add(90);
+        treap2.delete(7);
         System.out.println(treap2.getMin().getKey());
-        Integer kth = treap2.kthSmallestElement(2);
+        System.out.println(treap2.getMax().getKey());
+        Integer kth = treap2.kthSmallestElement(4);
         if(kth!=null)
             System.out.println(kth.intValue());
-
-
-
+        System.out.println(treap2.orderedKeys());
     }
 }
