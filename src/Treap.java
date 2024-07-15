10a2f80513584a491bed323deca4677240d81d95
Keinazj <jafari.keinaz@gmail.com>
2024-07-15 13:25:59 +0330

    add a search method and change the main

--- src/Treap.java
+++ src/Treap.java
@@ -1,14 +1,44 @@
-
 public class Treap<Key extends Comparable<Key>>{
     private Node<Key> root;
     private Node<Key> min;
     private Node<Key> max;
 
+
+    /**
+     * This method searches for a specific node in the treap, based on the key value
+     * @param root
+     * @param val
+     * @return the node
+     */
+    public Node<Key> treapSearch(Node<Key> root, Key val){
+        if (root==null)
+            return root;
+        int cmp = val.compareTo(root.getKey());
+        if(cmp<0)
+            return treapSearch(root.getLeftChild(),val);
+        else if (cmp>0)
+            return treapSearch(root.getRightChild(),val);
+        return root;
+    }
+
+    /**
+     * Add a Key to the Treap.
+     * @param key The key to add to the tree.
+     * @return A node containing the added key.
+     */
     public Node<Key> add(Key key){
         this.root = add_helper(key,this.root, null, null);
         return root;
     }
 
+    /**
+     * Helper function for add. Recursively adds a Node with the given Key to the Treap and finally Heapifys the added node using rotations.
+     * @param key The key to add to the Treap.
+     * @param root Treap root node.
+     * @param pre The predecessor of the node.
+     * @param suc The successor of the node.
+     * @return A node containing the added key.
+     */
     private Node<Key> add_helper(Key key, Node<Key> root, Node<Key> pre, Node<Key> suc) {
         if (root == null){
             Node<Key> newNode =  new Node<Key>(key);
@@ -64,6 +94,12 @@ public class Treap<Key extends Comparable<Key>>{
         return root;
     }
 
+    /**
+     * updates the successor and predecessor of a given node
+     * @param node changes affect this node
+     * @param pre the predecessor to assign the node
+     * @param suc the successor to assign the node
+     */
     public void updateSucAndPre(Node<Key> node , Node<Key> pre, Node<Key> suc){
         if (pre != null){
             pre.setSuccessor(node.getSuccessor());
@@ -73,11 +109,33 @@ public class Treap<Key extends Comparable<Key>>{
         }
     }
 
-    public Node<Key> delete(Key key){
-        this.root = delete_helper(key,this.root);
-        return root;
+    /**
+     * returns the k-th smallest element in the Treap.
+     @param  k   index + 1 of the desired element.
+     */
+    public Key kthSmallestElement(int k){
+        Node<Key> tmp = min;
+        for (int i = 0; i < k-1; i++){
+            tmp = tmp.getSuccessor();
+        }
+        return tmp.getKey();
     }
 
+    /**
+     * Delete a key from the given Treap.
+     * @param key key to be removed from the Treap.
+
+     */
+    public void delete(Key key){
+        this.root = delete_helper(key,this.root);
+    }
+
+    /**
+     * Helper function to delete a key from a Treap.
+     * @param key Key to be removed from the Treap.
+     * @param root The root of the Treap.
+     * @return returns the root node of the Treap. Null if Treap is empty.
+     */
     private Node<Key> delete_helper(Key key, Node<Key> root) {
         if (root == null){
             return root;
@@ -128,6 +186,31 @@ public class Treap<Key extends Comparable<Key>>{
         }
 
         return root;
+    }
+
+    /**
+     * get the height of the Treap. 0 can either mean that the Treap is empty or has only a single Node in it.
+     * @return Treap height.
+     */
+    public int getTreapHeight(){
+        if (this.root != null){
+            return root.getHeightFromBottom();
+        }
+        return 0;
+    }
+
+    public Key getTreapMin(){
+        if (this.min!=null){
+            return min.getKey();
+        }
+        return null;
+    }
+
+    public Key getTreapMax(){
+        if (this.max!=null){
+            return max.getKey();
+        }
+        return null;
     }
 
     public Node<Key> getRoot() {
--- src/main.java
+++ src/main.java
@@ -1,42 +1,16 @@
 public class main {
     public static void main(String[] args) {
-        Treap<Integer> treap = new Treap<>();
 
-        // Insert nodes
-        treap.add(10);
-        treap.add(20);
-        treap.add(5);
-        treap.add(15);
-        treap.add(25);
-
-        // Print the root and its left and right children
-        System.out.println("After insertions:");
-        printTreap(treap.getRoot());
-
-        // Delete a node
-        treap.delete(20);
-
-        // Print the root and its left and right children after deletion
-        System.out.println("After deletion of 20:");
-        printTreap(treap.getRoot());
-    }
-
-    private static void printTreap(Node<Integer> node) {
-        if (node == null) return;
-
-        System.out.println("Node: " + node.getKey() + ", Priority: " + node.getPriority() + ", SubtreeSize: " + node.getSubtreeSize() + ", Height: " + node.getHeightFromBottom());
-        if (node.getLeftChild() != null) {
-            System.out.println("  Left Child: " + node.getLeftChild().getKey());
-        } else {
-            System.out.println("  Left Child: null");
+        int sum = 0;
+        for (int j = 0; j< 1000; j++) {
+            Treap<Integer> treap = new Treap<>();
+            for (int i = 0; i < 100000; i++) {
+                treap.add(i);
+            }
+            System.out.println(treap.getTreapHeight());
+            sum += treap.getTreapHeight();
         }
-        if (node.getRightChild() != null) {
-            System.out.println("  Right Child: " + node.getRightChild().getKey());
-        } else {
-            System.out.println("  Right Child: null");
-        }
-        if (node.getLeftChild() != null) printTreap(node.getLeftChild());
-        if (node.getRightChild() != null) printTreap(node.getRightChild());
+        System.out.println(sum/1000);
+
     }
 }
-
